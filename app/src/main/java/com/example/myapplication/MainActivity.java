4b4f56ab369b3de1a6c6da6800914e1ccdf82e31
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2020-08-29 13:47:39 +0200

    Create second Activity and Intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,45 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    private EditText editName;
+    private EditText editPassword;
+    private Button buttonConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.editName);
+        editPassword = findViewById(R.id.editPassword);
+        buttonConfirm = findViewById(R.id.buttonConfirm);
+    }
+
+    public void onClickCreateOrder(View view)
+    {
+        ///trim() - delete space
+        String name = editName.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+        if(name.length() == 0 || password.length() == 0)
+        {
+
+        }
+        else
+        {
+            Intent intent = new Intent(this,CreateOrderActivity.class);
+            intent.putExtra("name",name);
+            intent.putExtra("password",password);
+            startActivity(intent);
+        }
+
     }
 }
